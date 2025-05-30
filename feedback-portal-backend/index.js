53be8c0746d92a322eea1791d2687cf1c5cdfe2f
unknown <nashnachiket19@gmail.com>
2025-05-30 14:10:09 +0530

    cors

--- feedback-portal-backend/index.js
+++ feedback-portal-backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   'http://localhost:3000',          
-  'https://your-frontend-url.com'   
+  'https://employee-feedback-portal-xvdd.vercel.app/'   
 ];
 
 app.use(cors({
