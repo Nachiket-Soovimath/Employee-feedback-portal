4c02a2c198d2fd12789b5f16f71e0ec41d932fd3
unknown <nashnachiket19@gmail.com>
2025-05-30 14:19:51 +0530

    cors

--- feedback-portal-backend/index.js
+++ feedback-portal-backend/index.js
@@ -6,20 +6,19 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+
 const allowedOrigins = [
-  'http://localhost:3000',          
-  'https://employee-feedback-portal-xvdd.vercel.app',   
+  'http://localhost:3000',
+  'https://employee-feedback-portal-xvdd.vercel.app',
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-   
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-    return callback(null, true);
   }
 }));
 
